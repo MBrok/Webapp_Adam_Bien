a9f4d89731abd39c1a824ee6fc2bef4d0597b675
Michael Brockhaus <michael@michael-brockhaus.de>
2021-02-27 16:53:07 +0100

    Nach Video 26

--- testProjekt/test/navigation.spec.js
+++ testProjekt/test/navigation.spec.js
@@ -3,13 +3,11 @@ context('navigation',()=> {
         cy.visit("http://localhost:3000");
     });
 
-    const view = 'add';
-    it('navigation by click', () => {
-        cy.get(`[href="#${view}"]`).click().should("have.class","a-link");
-
-        cy.get('air-crumb').should('contain',view);
-        cy.get(`[href="#list"]`).click().should("have.class","a-link");
-        cy.get(`[href="#${view}"]`).should("not.have.class","a-link");
+    const views = ['overview','add','list','about'];
+    views.forEach( view => {
+        it(`navigation by click -> ${view}`, () => {
+            cy.get(`[href="#${view}"]`).click().should("have.class","a-link");
+            cy.get('air-crumb').should('contain',view);
+        });
     });
-
 });
